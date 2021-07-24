691f138a4e717730440c5ec52373b516282903fe
hbhavsar01 <hbhavsar01@hotmail.com>
2021-07-24 16:17:41 -0400

    Minor file formatting...

--- MaySeleniumFramework/src/main/java/businessfunctions/BaseClass.java
+++ MaySeleniumFramework/src/main/java/businessfunctions/BaseClass.java
@@ -6,15 +6,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import configuration.Configuration;
 
 public class BaseClass {
-	
+
 	public static WebDriver driver = null;
 	public static void openBrowser() {
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\Shriyan\\eclipse-workspace\\MaySeleniumFramework\\Drivers\\chromedriver.exe");
-	driver= new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Shriyan\\eclipse-workspace\\MaySeleniumFramework\\Drivers\\chromedriver.exe");
+		driver= new ChromeDriver();
 
-	driver.get(Configuration.getProperty("url"));
+		driver.get(Configuration.getProperty("url"));
 
-}
+	}
 	public static void CloseBrowser() {
 		driver.close();
 		driver.quit();
